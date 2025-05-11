2c8af092557f0e13760aa3ecdf6aafb90ae297df
Viktor Velychkocd <velviktor831@gmail.com>
2025-05-11 11:58:51 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid command")
         return
-
     source_file = parts[1]
     destination_file = parts[2]
 
     if source_file == destination_file:
+        print("Source and destination cannot be the same")
         return
-
     try:
         with (open(source_file, "r") as file_in,
               open(destination_file, "w") as file_out):
